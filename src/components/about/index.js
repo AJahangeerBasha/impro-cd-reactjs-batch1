74c793606e8bec706a327850bee0ec741829d019
jbash2 <Jahangeer.Basha@aexp.com>
2017-01-25 12:58:59 +0530

    Add React-Router and Bootstrap and configured those in the App.

---
+++ src/components/about/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <h1>this is the about page</h1>
+    );
+  }
+}
+
+About.defaultProps = {
+};
+
+export default About;
---
+++ src/components/home/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="index">
+        <div className="notice">
+          <h1>this is the home page</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+Home.defaultProps = {
+};
+
+export default Home;
