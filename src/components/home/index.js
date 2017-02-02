26deaaf359151630204873ec2d3ab4ab2f7ac3f0
unknown <dindayal.dhakar@IMPETUS-NL172.impetus.co.in>
2017-02-02 15:53:38 +0530

    dragable functionality on dashboard page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,11 +1,100 @@
 import React from 'react';
-
+import jQuery from 'jquery';
+import jQueryui from 'jquery-ui/ui/widgets/sortable';
 class Home extends React.Component {
   render() {
+	  jQuery(function($) {
+        var panelListTodo = $('#draggablePanelTodoList');
+		var panelListInProgress = $('#draggablePanelInProgressList');
+		var panelListDone = $('#draggablePanelDoneList');
+		
+        panelListTodo.sortable({
+            // Only make the .panel-heading child elements support dragging.
+            // Omit this to make then entire <li>...</li> draggable.
+            handle: '.panel-heading', 
+            update: function() {
+                $('.panel', panelListTodo).each(function(index, elem) {
+                     var $listItem = $(elem),
+                         newIndex = $listItem.index();
+
+                     // Persist the new indices.
+                });
+            }
+        });
+		panelListInProgress.sortable({
+            // Only make the .panel-heading child elements support dragging.
+            // Omit this to make then entire <li>...</li> draggable.
+            handle: '.panel-heading', 
+            update: function() {
+                $('.panel', panelListInProgress).each(function(index, elem) {
+                     var $listItem = $(elem),
+                         newIndex = $listItem.index();
+
+                     // Persist the new indices.
+                });
+            }
+        });
+		panelListDone.sortable({
+            // Only make the .panel-heading child elements support dragging.
+            // Omit this to make then entire <li>...</li> draggable.
+            handle: '.panel-heading', 
+            update: function() {
+                $('.panel', panelListDone).each(function(index, elem) {
+                     var $listItem = $(elem),
+                         newIndex = $listItem.index();
+
+                     // Persist the new indices.
+                });
+            }
+        });
+    });
     return (
       <div className="index">
         <div className="notice">
-          <h1>this is the home page</h1>
+          <h1>this is the Dashboard page</h1>
+			
+			<div className="row">
+			  <div className="col-sm-6 col-md-4 thumbnail">
+				<h3>Todo</h3>
+				<ul className="list-unstyled" id="draggablePanelTodoList">
+					<li className="panel panel-info">
+						<div className="panel-heading">You can drag this panel.</div>
+						<div className="panel-body">Content ...</div>
+					</li>
+					<li className="panel panel-info">
+						<div className="panel-heading">You can drag this panel too.</div>
+						<div className="panel-body">Content ...</div>
+					</li>
+				</ul>
+			  </div>
+			  <div className="col-sm-6 col-md-4 thumbnail">
+				<h3>Inprogress</h3>
+				<ul className="list-unstyled" id="draggablePanelInProgressList">
+					<li className="panel panel-info">
+						<div className="panel-heading">You can drag this panel.</div>
+						<div className="panel-body">Content ...</div>
+					</li>
+					<li className="panel panel-info">
+						<div className="panel-heading">You can drag this panel too.</div>
+						<div className="panel-body">Content ...</div>
+					</li>
+				</ul>
+			  </div>
+			  <div className="col-sm-6 col-md-4 thumbnail">
+				<h3>Done</h3>
+				<ul className="list-unstyled" id="draggablePanelDoneList">
+					<li className="panel panel-info">
+						<div className="panel-heading">You can drag this panel.</div>
+						<div className="panel-body">Content ...</div>
+					</li>
+					<li className="panel panel-info">
+						<div className="panel-heading">You can drag this panel too.</div>
+						<div className="panel-body">Content ...</div>
+					</li>
+				</ul>
+			  </div>
+			</div>
+			
         </div>
       </div>
     );
